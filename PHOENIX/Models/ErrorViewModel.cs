6fe21600fb7d208e7b6ddf8cf9880adaf237b530
artem <artem@ARTEM>
2026-03-27 14:12:02 +0200

    Add project files.

---
+++ PHOENIX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PHOENIX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
